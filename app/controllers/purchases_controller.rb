393edc8746426641788ec09fe153294551197fa5
bre0711 <brezelgukptp0@gmail.com>
2020-09-10 19:31:57 +0900

    修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -39,6 +39,8 @@ class PurchasesController < ApplicationController
 
   def move_to_root_path
     @item = Item.find(params[:item_id])
-    redirect_to root_path unless user_signed_in? && @item.user_id == current_user.id && @item.user_id != current_user.id 
+    redirect_to root_path unless user_signed_in?
+    redirect_to root_path unless @item.user_id != current_user.id 
+    redirect_to root_path unless @item.user_id == current_user.id 
   end
 end
