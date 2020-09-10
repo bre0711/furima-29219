64dcd46961469cdae86edd50dc95775133ed3799
bre0711 <brezelgukptp0@gmail.com>
2020-09-10 18:52:23 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,6 +52,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to user_registration_path unless user_signed_in?
   end
 end
